c28e1d28aa20800240d2a6d60911a0887cb2dcb2
Shaimaa Ali <shaymaali121@gmail.com>
2024-08-30 17:35:26 +0300

    html helper updates

--- BookingSystem/Utility/HtmlHelpers.cs
+++ BookingSystem/Utility/HtmlHelpers.cs
@@ -13,6 +13,21 @@ namespace BookingSystem.Utility
 
             return (controller == routeController && action == routeAction) ? "active" : "";
         }
+        public static string TestimonialStatus(this IHtmlHelper html, string status)
+        {
+            return status.Equals(SD.Testimonial_Pending) ? "table-warning" : status.Equals(SD.Testimonial_Approved) ? "table-success" : "table-danger";
+        }
+        public static string BookingStatus(this IHtmlHelper html, string status)
+        {
+            return status.Equals(SD.BookingStatus_Confirmed) ?
+                "badge badge-success" :
+                status.Equals(SD.BookingStatus_Cancelled) ?
+                "badge badge-danger" :
+                status.Equals(SD.BookingStatus_Pending) ?
+                "badge badge-warning" :
+                status.Equals(SD.BookingStatus_CheckedOut) || status.Equals(SD.BookingStatus_CheckedIn) ?
+                "badge badge-info":"";
+        }
     }
 
 }
